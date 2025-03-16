eb3373390ef2b61e7ac13f8eb5d6a78ca31fdbe8
erickyuste <erickpulgayuste@gmail.com>
2025-03-16 18:40:40 +0700

    fixing the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,6 +37,31 @@ function Navbar() {
     };
   }, [levelsDropdownRef]);
 
+  const toggleLevelsDropdown = () => {
+    setIsLevelsDropdownOpen(!isLevelsDropdownOpen);
+  };
+
+  useEffect(() => {
+    setIsLevelsDropdownOpen(false);
+    setIsNavDropdownOpen(false);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        levelsDropdownRef.current &&
+        !levelsDropdownRef.current.contains(event.target)
+      ) {
+        setIsLevelsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [levelsDropdownRef]);
+
   return (
     <div className="w-full">
       <nav>
@@ -86,13 +111,13 @@ function Navbar() {
           <div
             className={`${
               isNavDropdownOpen ? "block" : "hidden"
-            } md:block absolute md:relative w-[50%] right-10 top-16 md:top-0 md:w-auto`}
+            } absolute md:relative md:top-0 w-[50%] right-11 top-11 md:block md:w-auto`}
             id="navbar-dropdown"
           >
-            <ul className="flex flex-col gap-6 items-center justify-center font-bold p-4 md:p-0 mt-4  bg-[#006494] md:bg-[#051923] rounded-2xl rounded-tr-none transition-all duration-200 ease-in-out origin-top-right md:flex-row md:mt-0 md:border-0">
+            <ul className="flex flex-col items-center font-medium bg-[#006494] md:bg-transparent p-4 md:p-0 mt-4 border rounded-xl rounded-tr-none md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
                 <Link
-                  to="/"
+                  to="/home"
                   className="block py-2 px-3 font-bold text-white text-center rounded-xl hover:bg-white w-[5rem] hover:text-[#006494] transition-all duration-300 ease-in-out md:bg-transparent md:p-0 md:dark:text-blue-500 md:dark:bg-transparent"
                   aria-current="page"
                 >
@@ -108,105 +133,119 @@ function Navbar() {
                   About
                 </Link>
               </li>
-
-              <button
-                id="dropdownDefaultButton"
-                data-dropdown-toggle="dropdown"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-              >
-                Choose a level{" "}
-                <svg
-                  class="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
+              <li className="relative">
+                <button
+                  id="dropdownNavbarLink"
+                  data-dropdown-toggle="dropdownNavbar"
+                  className="flex items-center justify-between w-full font-bold hover:border-white cursor-pointer"
+                  onClick={toggleLevelsDropdown}
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
-              </button>
+                  <span className="flex items-center justify-center rounded-md p-2 bg-[#006494] w-[10rem] transition-all duration-100 ease-in-out text-white hover:border-2  md:border-none border-1 border-gray-100">
+                    Choose a level
+                    <svg
+                      className="w-2.5 h-2.5 ms-2.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 10 6"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m1 1 4 4 4-4"
+                      />
+                    </svg>
+                  </span>
+                </button>
 
-              <div
-                id="dropdown"
-                class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
-              >
-                <ul
-                  class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownDefaultButton"
+                <div
+                  ref={levelsDropdownRef}
+                  id="dropdownNavbar"
+                  className={`${
+                    isLevelsDropdownOpen ? "block" : "hidden"
+                  } z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600 absolute top-full left-0`}
                 >
-                  <li>
-                    <Link
-                      to="/kinder"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Kinder
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/pre-p"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Pre-Primary
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/g1"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Grade 1
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/g2"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Grade 2
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/g3"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Grade 3
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/g4"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Grade 4
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/g5"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Grade 5
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/g6"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Grade 6
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+                  <ul
+                    className="py-2 text-sm text-gray-700 w-full rounded-lg"
+                    aria-labelledby="dropdownLargeButton"
+                  >
+                    <li>
+                      <Link
+                        to="/kinder"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        Kinder
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/pre-p"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        Pre-P
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/g1"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        G1
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/g2"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        G2
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/g3"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        G3
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/g4"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        G4
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/g5"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        G5
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/g6"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={closeLevelsDropdown}
+                      >
+                        G6
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
