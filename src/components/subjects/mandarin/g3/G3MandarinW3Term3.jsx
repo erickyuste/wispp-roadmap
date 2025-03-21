b0483209d32ba8e57fd0467ac6face82838e190c
erickyuste <erickpulgayuste@gmail.com>
2025-03-21 11:46:09 +0700

    added  mandarin lessons for term 3

--- src/components/subjects/mandarin/g3/G3MandarinW3Term3.jsx
+++ src/components/subjects/mandarin/g3/G3MandarinW3Term3.jsx
@@ -11,6 +11,17 @@ function G3MandarinW3Term3() {
           • Students talk about their favourite sports and hobbies.
         </p>
         <br></br>
+        <p className="font-semibold text-xs md:text-[16px] sm:text-sm text-white ">
+          • 目标：Lesson 6-1 P68-69 Mandarin-W3-T3（G3&4） 1 复习Lesson 5
+          所学的主要内容。 2 学习如何用中文说出自己家庭成员的名字。 3
+          学习如何询问他人的家庭成员一共有几口人 4
+          学习如何回答自己的家庭成员一共有几口人 课堂活动： 1
+          老师为每个学生打印一张家谱树，学生在家谱树上填写自己的家庭成员。 2
+          每人带一张全家福的照片，之后两人一组轮流练习如何向他人介绍自己的家庭成员。
+          作业： 1 练习写关于家庭成员的汉字。 2
+          完成练习簿上有关家庭成员的部分内容。
+        </p>
+        <br></br>
 
         <div className="gap-2 mt-2 flex flex-wrap">
           <button className="w-[6em] h-[2em] rounded-xl text-xs md:text-sm text-white border-2 border-white font-bold hover:text-[#0582ca] hover:bg-white">
