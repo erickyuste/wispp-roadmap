1aa3ce217c21fd301b048cc5040462d24597a96a
erickyuste <erickpulgayuste@gmail.com>
2025-03-27 07:19:44 +0700

    added Tfiles on Arts G2

--- src/components/subjects/visual arts/g2/G2VisualArtsSeedTerm4.jsx
+++ src/components/subjects/visual arts/g2/G2VisualArtsSeedTerm4.jsx
@@ -77,6 +77,15 @@ function G2VisualArtsSeedTerm4() {
               Create LP
             </a>
           </button>
+          <button className="w-[5em] h-[2em] rounded-xl text-xs md:text-sm text-white border-2 border-white font-bold hover:text-[#0582ca] hover:bg-white">
+            <a
+              href="https://docs.google.com/document/d/1S11grUi92e106DZCu0jbsKgHrc8lEuEo07uChu0K_HU/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              TFiles
+            </a>
+          </button>
         </div>
       </div>
     </div>
