3513faeb242a0e60959915f592a092c900ab7dbf
erickyuste <erickpulgayuste@gmail.com>
2025-04-01 13:29:25 +0700

    added clickable terms

--- src/components/grade2main/Term1.jsx
+++ src/components/grade2main/Term1.jsx
@@ -46,8 +46,14 @@ function Term1() {
       <div className="w-[95vw] flex justify-evenly items-center">
         <div className="w-[100%] bg-[#003554] h-[1px] rotate-180"></div>
         <li className="flex items-center justify-center shadow-lg list-none w-[100%] rounded-lg bg-[#003554]">
-          <h1 className="title py-2 font-bold text-xs sm:text-sm md:text-lg lg:text-xl text-gray-300 text-center">
-            TERM 1
+          <h1 className="title flex gap-1 items-center justify-center py-2 font-bold text-xs sm:text-sm md:text-lg lg:text-xl text-gray-300 text-center cursor-pointer transition-transform duration-200 hover:scale-120">
+            <a
+              href="https://docs.google.com/spreadsheets/d/1WGzQQW0bUTwRJ8495DPhu7OzDD_QKs-zQe2ovIHLHPE/edit?usp=sharing"
+              target="_blank"
+            >
+              TERM 1
+            </a>
+            <img src={click} className="w-[2rem] filter invert" />
           </h1>
         </li>
         <div className="w-[100%] bg-[#003554] h-[1px] rotate-180"></div>
